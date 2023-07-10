ed5da765cf49966beeeae22e4c899b16b1fee425
“AmbatiRamesh” <rameshkohli401@gmail.com>
2023-07-10 10:37:38 +0530

    Added: Ability to dequeue from the beginning

--- src/main/java/com/bridgelabz/stackandqueue/StackAndQueue.java
+++ src/main/java/com/bridgelabz/stackandqueue/StackAndQueue.java
@@ -5,15 +5,25 @@ public class StackAndQueue {
     public void enQueue(Object data) {
         lists.addFirst(data);
     }
+    public void deQueue() {
+        lists.deleteFirst();
+    }
     public void display() {
         lists.display();
     }
     public static void main(String[] args) {
-        System.out.println("welcome to Queue program");
+        System.out.println("Welcome to Stack and Queue Data structure");
         StackAndQueue queue = new StackAndQueue();
         queue.enQueue(70);
         queue.enQueue(30);
         queue.enQueue(56);
+        queue.display();
+        queue.display();
+        queue.deQueue();
+        queue.display();
+        queue.deQueue();
+        queue.display();
+        queue.deQueue();
         queue.display();
     }
 }
